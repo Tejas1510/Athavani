74650e229904689ac9cd4c6d8ac10ee12f181f21
lakshya Mishra <68530218+kenkirito@users.noreply.github.com>
2020-12-27 04:41:28 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,3 +7,4 @@ export const createPost = (newPost) =>axios.post(url,newPost)
 export const updatePost = (id,updatePost) =>axios.patch(`${url}/${id}`,updatePost);
 export const deletePost = (id) =>axios.delete(`${url}/${id}`);
 export const likePost = (id) =>axios.patch(`${url}/${id}/likePost`)
+export const dislikePost = (id) =>axios.patch(`${url}/${id}/dislikePost`)

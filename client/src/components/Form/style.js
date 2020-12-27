74eba4038c6993f9747ac40533667675d0e37a1e
Aniket Dewnani <aniketdewnani31@gmail.com>
2020-12-27 16:49:45 +0530

    Changed Layout Of The Form

--- client/src/components/Form/style.js
+++ client/src/components/Form/style.js
@@ -7,7 +7,21 @@ export default makeStyles((theme) => ({
     },
   },
   paper: {
+    borderRadius: '12px',
+    background: 'linear-gradient(180deg, rgb(255 192 146 / 85%) 48%, rgb(253 226 52 / 0.71) 100%)',
+  },
+  heading: {
+    background: 'rgb(248 172 78) 58%;',
+    borderRadius: '12px',
+    width: '380px!important',
+    height: '55px',
+  },
+  contents: {
     padding: theme.spacing(2),
+  },
+  inputBox: {
+    marginBottom: '10px',
+    background: '#FFFFF1',
   },
   form: {
     display: 'flex',
@@ -20,5 +34,10 @@ export default makeStyles((theme) => ({
   },
   buttonSubmit: {
     marginBottom: 10,
+    background:'#FFB800',
+    color:'white', 
+    bprderRadius: '7px', 
+    width: '149px',
+    marginLeft: '90px', 
   },
 }));

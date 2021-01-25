b736375a27abc3f661cb46168494063db6c5bfb0
Tejas1510 <64543913+Tejas1510@users.noreply.github.com>
2021-01-25 14:10:43 +0530

    Revert "Forgot Password Backend"

--- server/controller/authentication.js
+++ server/controller/authentication.js
@@ -1,7 +1,6 @@
 import  User from '../modules/user.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import nodemailer from 'nodemailer';
 
 export const signup = async (req, res) => {
     try {
@@ -66,84 +65,6 @@ export const verify = async (req, res) => {
         return res.status(200).json({id:isVerified._id, name:user.name});
 
     } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
-
-export const forgot = async (req, res) => {
-    try {
-        const { email } = req.body;
-
-        const user = await User.findOne({email: email});
-        if(!user) {
-            return res.status(403).json(`No User Found with email: ${email}`);
-        }
-
-        const token = jwt.sign({_id: user._id}, process.env.RESET_PASSWORD_JWT_KEY, {expiresIn: '15m'});
-        if(!token) {
-            return res.status(403).json("Token Error!");
-        }
-
-        let transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-              user: process.env.MAIL,
-              pass: process.env.PASS,
-            },
-        });
-
-        await user.updateOne({resetPassLink: token});
-
-        await transporter.sendMail({
-            from: `${process.env.MAIL}`, // sender address
-            to: `${email}`, // list of receivers
-            subject: "Reset Password || Athavani", // Subject line
-            html: `<div style="text-align: center; background-color: #ffa500; padding: 11px">
-            <h1>You have requested to reset your Password</h1>
-            <p style="padding: 15px 0;">
-                 Hello ${user.name}, We received a request to reset the password for your account for this email address. To initiate the password reset process for your account, click the button below. Link will expire in 15 minutes.
-            </p>
-            <a href="${process.env.CLIENT_URL}/resetPassword/${token}" target="_blank" style="text-decoration: none; background-color: tomato; color: white; padding: 1rem 1.5rem; border-radius: 25px; box-shadow: 0px 1px 3px black">
-                  Reset Password
-            </a>
-            <p style="padding: 15px 0;">If you did not make this request, you can simply ignore this email.</p>`, // html body
-            });
-
-        res.status(200).json("Email Sent");
-
-    } catch (error) {
-        res.status(404).json({message: error.message});
-    }
-}
-
-export const resetPassword = async (req, res) => {
-    try {
-        const {token, newPassword} = req.body;
-
-        if(!token) {
-            res.status(401).json("Token not found!");
-        }
-
-        const isVerified = await jwt.verify(token, process.env.RESET_PASSWORD_JWT_KEY);
-
-        if(!isVerified) {
-            res.status(401).json("Invalid Token or it is expired!");
-        }
-
-        const user = await User.findOne({resetPassLink: token});
-
-        if(!user) {
-            res.status(404).json("User not Found!");
-        }
-
-        const hash = await bcrypt.hash(newPassword, 12);
-        await user.updateOne({password: hash});
-
-        res.status(200).json("Password Changed Successfully.");
-    }
-    catch (error) {
         res.status(404).json({message: error.message});
     }
 }

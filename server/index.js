1ba594164c262ec232b72bb80804d625babdd069
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-03-25 09:59:30 +0530

    body-parser dependency removed and made changes to index.js in server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
@@ -11,8 +10,8 @@ import cron from 'node-cron';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 dotenv.config();
 app.use('/posts', postRoutes);

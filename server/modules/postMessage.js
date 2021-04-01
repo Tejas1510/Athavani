9315572a35b1524a02f207b60042bb3a5cd7b0b8
Ekjot Kaur <66993173+EkjotKaur@users.noreply.github.com>
2021-04-01 21:01:43 +0530

    Comment time (#170)
    * Like and Unlike Issue Solved
    
    * created at time in Comments
    
    * comment time positon changed

--- server/modules/postMessage.js
+++ server/modules/postMessage.js
@@ -1,63 +1,66 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    title: String,
-    message: String,
-    creator: {
-        _id: {
-            type: mongoose.Types.ObjectId,
-            ref: 'User',
-            required: [true, 'Creator is Required']
-        },
-        name: {
-            type: String,
-            required: [true, 'Creator Name is Required']
-        }
+  title: String,
+  message: String,
+  creator: {
+    _id: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Creator is Required"],
     },
-    tags: [String],
-    selectedFile: String,
-    likes: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
-    dislikes: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
-    favorites: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
-    comments: [
-        {
-            postedBy: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User'
-            },
-            img: {
-                type: String
-            },
-            name: {
-                type: String,
-                required: [true, 'Creator Name is Required']
-            },
-            message: {
-                type: String
-            }
-        }
-    ],
-    createdAt: {
+    name: {
+      type: String,
+      required: [true, "Creator Name is Required"],
+    },
+  },
+  tags: [String],
+  selectedFile: String,
+  likes: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  dislikes: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  favorites: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  comments: [
+    {
+      postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      img: {
+        type: String,
+      },
+      name: {
+        type: String,
+        required: [true, "Creator Name is Required"],
+      },
+      message: {
+        type: String,
+      },
+      createdAt: {
         type: Date,
-        default: new Date(),
+      },
     },
-})
+  ],
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-var PostMessage = mongoose.model('PostMessage', postSchema);
+var PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;

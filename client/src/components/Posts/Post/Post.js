1d3da50b9b4d986b776d8111c515a2cbc0b89c1a
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-04-16 16:24:06 +0530

    Empty comment issue solved

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -512,6 +512,9 @@ const Post = ({ post, setCurrentId, fromProfile }) => {
                 disabled={commented}
                 onClick={() => {
                   console.log("Clicked!");
+                  if (!commentMessage) {
+                    return toast.error("Comment cannot be empty.");
+                  }
                   setCommented(true);
                   dispatch(
                     commentPost(post._id, {

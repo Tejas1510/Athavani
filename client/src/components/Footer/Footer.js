f34bfd5229766ed9798f4a8211eef83e988c8a2f
Cgoyal48 <51297729+Cgoyal48@users.noreply.github.com>
2021-05-08 00:37:53 -0700

    ✨ Left Navbar (#219)

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -11,23 +11,7 @@ function Footer() {
     <div>
       <footer>
         <div style={{ position: "relative" }}>
-          <svg
-            viewBox="0 -20 700 110"
-            width="100%"
-            height="50px"
-            preserveAspectRatio="none"
-            style={{ position: "absolute", top: "110px" }}
-          >
-            <path
-              transform="translate(0, -20)"
-              d="M0,10 c80,-22 240,0 350,18 c90,17 260,7.5 350,-20 v50 h-700"
-              fill="#FFAA00"
-            />
-            <path
-              d="M0,10 c80,-18 230,-12 350,7 c80,13 260,17 350,-5 v100 h-700z"
-              fill="#FF7F50"
-            />
-          </svg>
+          
           <div
             class="footer-row"
            >

708b9ba3ba44564bb5820f629478027984dc33b4
Ameya Jain <66305085+AmeyaJain-25@users.noreply.github.com>
2021-05-29 16:19:00 +0530

    Footer position bug (#257)

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
   return (
     <div>
       <footer>
-        <div style={{ position: "relative" }}>
+        <div>
           
           <div
             class="footer-row"

89729e8d2653898f20dde45fea82df1b3879db61
Ameya Jain <66305085+AmeyaJain-25@users.noreply.github.com>
2021-06-05 14:17:50 +0530

    Long Content is now handled (#280)

--- client/src/components/Posts/style.js
+++ client/src/components/Posts/style.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   mainContainer: {
     display: 'flex',
-    alignItems: 'center',
+    alignItems: 'baseline',
   },
   smMargin: {
     margin: theme.spacing(1),
